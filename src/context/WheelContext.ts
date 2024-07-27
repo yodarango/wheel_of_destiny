0d5f2a1fdd77b671f49a005416675f44fb69bcc5
daniel rangel <yodarango@Rooky.home>
2024-07-27 19:09:14 -0400

    set up context

---
+++ src/context/WheelContext.ts
@@ -0,0 +1,64 @@
+import { createContext } from "react";
+import { TSlice } from "../types";
+
+export const defaultWheelColors = [
+  "#f8b195",
+  "#f67280",
+  "#ffc1d3",
+  "#d4c0e5",
+  "#1d4260",
+];
+
+export const defaultState = {
+  slicesData: ["One", "Two", "Three", "Four"],
+  wheelColors: defaultWheelColors,
+  wheelTitle: "WHEEL OF DESTINY 🎡",
+  isShowingWheelsList: false,
+  isWheelSpinning: false,
+  currentWheelId: "",
+  wheelThemeId: -1,
+  savedSlices: {},
+  wheelStatus: 1,
+};
+
+export type TWheelContext = {
+  state: {
+    isShowingWheelsList: boolean; // opens the drawer that shows the list of saved wheels
+    currentWheelId: string; // the current selected wheel
+    isWheelSpinning: false; // if the wheel is spinning
+    wheelColors: string[]; // the selected colors of the wheel. Probably should get rid of this and handle it with the themeId
+    slicesData: string[]; // the strings that will be displayed in the wheel
+    wheelThemeId: number; // the selected theme of the wheel
+    isWheelSaved: number; // whether the wheel is saved or not
+    wheelTitle: string; // the title of the wheel
+    savedSlices: {}; // the saved slices to local storage
+  };
+  handleChangeColor: (data: { colors: string[]; id: number }) => {};
+  handleOrderSlices: (isOrderAtoZ: boolean) => {};
+  handleRemoveItemFromList: (index: number) => {};
+  handleSelectASavedWheel: (slice: TSlice) => {};
+  handleUpdateSlices: (slices: string[]) => {};
+  handleWheelTitle: (title: string) => {};
+  handleRemoveWheel: (id: string) => {};
+  handleToggleSpinningWheel: () => {};
+  handleCreateNewWheel: () => {};
+  handleShuffleSlices: () => {};
+  handleSave: () => {};
+};
+
+export const defaultContext = {
+  state: defaultState,
+  handleChangeColor: (_: { colors: string[]; id: number }) => {},
+  handleSelectASavedWheel: (_: TSlice) => {},
+  handleRemoveItemFromList: (_: number) => {},
+  handleUpdateSlices: (_: string[]) => {},
+  handleOrderSlices: (_: boolean) => {},
+  handleRemoveWheel: (_: string) => {},
+  handleToggleSpinningWheel: () => {},
+  handleWheelTitle: (_: string) => {},
+  handleCreateNewWheel: () => {},
+  handleShuffleSlices: () => {},
+  handleSave: () => {},
+};
+
+export const WheelContext = createContext(defaultContext);
---
+++ src/context/WheelContextProvider.tsx
@@ -0,0 +1,209 @@
+// WheelContext.tsx
+import React, { useState, useEffect, ReactNode } from "react";
+import { generateRandomCode } from "../lib/generateRandomCode";
+import { WheelContext, defaultWheelColors } from "./WheelContext";
+import { TSavedSlices, TSlice } from "../types";
+
+const WHEEL_STATUS_UNSAVED = 1;
+const WHEEL_STATUS_SAVED = 2;
+const WHEEL_STATUS_UPDATING = 3;
+
+export const WheelProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [wheelStatus, setWheelStatus] = useState<number>(WHEEL_STATUS_UNSAVED);
+  const [wheelTitle, setWheelTitle] = useState<string>("WHEEL OF DESTINY 🎡");
+  const [currentWheelId, setCurrentWheelId] = useState<string>("");
+  const [slicesData, setSlicesData] = useState<string[]>([
+    "One",
+    "Two",
+    "Three",
+    "Four",
+  ]);
+  const [wheelColors, setWheelColors] = useState<string[]>(defaultWheelColors);
+  const [isWheelSpinning, setIsWheelSpinning] = useState<boolean>(false);
+  const [wheelThemeId, setWheelThemeId] = useState<number>(-1);
+  const [isShowingWheelsList, setIsShowingWheelsList] =
+    useState<boolean>(false);
+  const [savedSlices, setSavedSlices] = useState<TSavedSlices>({});
+
+  useEffect(() => {
+    getLocalStorageData();
+  }, []);
+
+  const getLocalStorageData = () => {
+    const lastWheelUsed = localStorage.getItem("wod__last-wheel-used");
+    const wheelsSaved = localStorage.getItem("wod__wheels-saved");
+    if (lastWheelUsed && wheelsSaved) {
+      const wheelsSavedObj = JSON.parse(wheelsSaved);
+      const findWheelSaved = wheelsSavedObj[lastWheelUsed];
+
+      setWheelThemeId(findWheelSaved.themeId);
+      setWheelColors(findWheelSaved.colors);
+      setSlicesData(findWheelSaved.slices);
+      setWheelTitle(findWheelSaved.title);
+      setWheelStatus(WHEEL_STATUS_SAVED);
+      setCurrentWheelId(lastWheelUsed);
+      setSavedSlices(wheelsSavedObj);
+    } else {
+      setCurrentWheelId(generateRandomCode());
+    }
+  };
+
+  const handleUpdateSlices = (slices: string[]) => {
+    setSlicesData(slices);
+    handleUpdateWheelStatus(currentWheelId);
+  };
+
+  const handleWheelTitle = (title: string) => {
+    setWheelTitle(title);
+    handleUpdateWheelStatus(currentWheelId);
+  };
+
+  const handleSave = () => {
+    const wheelsSaved = localStorage.getItem("wod__wheels-saved");
+    const newWheel = {
+      themeId: wheelThemeId,
+      colors: wheelColors,
+      slices: slicesData,
+      title: wheelTitle,
+    };
+
+    let wheelToSave: Record<string, any> = {};
+
+    if (wheelsSaved) {
+      const wheelsSavedObj = JSON.parse(wheelsSaved);
+      wheelToSave = wheelsSavedObj;
+    }
+    wheelToSave[currentWheelId] = newWheel;
+    localStorage.setItem("wod__wheels-saved", JSON.stringify(wheelToSave));
+    localStorage.setItem("wod__last-wheel-used", currentWheelId);
+    setWheelStatus(WHEEL_STATUS_SAVED);
+    setSavedSlices(wheelToSave);
+  };
+
+  const handleCreateNewWheel = () => {
+    setCurrentWheelId(generateRandomCode());
+    setSlicesData(["One", "Two", "Three", "Four"]);
+    setWheelStatus(WHEEL_STATUS_UNSAVED);
+    setWheelTitle("WHEEL OF DESTINY 🎡");
+    setWheelColors(defaultWheelColors);
+    setWheelThemeId(-1);
+  };
+
+  const handleSelectASavedWheel = (data: TSlice) => {
+    setIsShowingWheelsList(!isShowingWheelsList);
+    setCurrentWheelId(data.id);
+    setWheelTitle(data.title);
+    setWheelStatus(WHEEL_STATUS_SAVED);
+    setSlicesData(data.slices);
+    setWheelColors(data.colors);
+    localStorage.setItem("wod__last-wheel-used", data.id);
+  };
+
+  const handleOrderSlices = (isOrderAtoZ: boolean) => {
+    const sortedSlices = [...slicesData].sort((a, b) =>
+      isOrderAtoZ ? a.localeCompare(b) : b.localeCompare(a)
+    );
+    setSlicesData(sortedSlices);
+    handleUpdateWheelStatus(currentWheelId);
+  };
+
+  const handleShuffleSlices = () => {
+    const shuffledSlices = [...slicesData];
+    for (let i = shuffledSlices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledSlices[i], shuffledSlices[j]] = [
+        shuffledSlices[j],
+        shuffledSlices[i],
+      ];
+    }
+    setSlicesData(shuffledSlices);
+    handleUpdateWheelStatus(currentWheelId);
+  };
+
+  const handleUpdateWheelStatus = (wheelId: string) => {
+    const wheelsSaved = localStorage.getItem("wod__wheels-saved");
+    if (wheelsSaved) {
+      const wheelsSavedObj = JSON.parse(wheelsSaved);
+      const findWheelSaved = wheelsSavedObj[wheelId];
+      setWheelStatus(
+        findWheelSaved ? WHEEL_STATUS_UPDATING : WHEEL_STATUS_UNSAVED
+      );
+    }
+  };
+
+  const handleChangeColor = (data: { colors: string[]; id: number }) => {
+    setWheelColors(data.colors);
+    setWheelThemeId(data.id);
+    handleUpdateWheelStatus(currentWheelId);
+  };
+
+  const handleRemoveItemFromList = (index: number) => {
+    const newSlices = slicesData.filter((_, i) => i !== index);
+    setSlicesData(newSlices);
+    handleUpdateWheelStatus(currentWheelId);
+  };
+
+  const handleRemoveWheel = (id: string) => {
+    const wheelsSaved = localStorage.getItem("wod__wheels-saved");
+    if (wheelsSaved) {
+      const wheelsSavedObj = JSON.parse(wheelsSaved);
+      delete wheelsSavedObj[id];
+      localStorage.setItem("wod__wheels-saved", JSON.stringify(wheelsSavedObj));
+
+      const lastWheelUsed = localStorage.getItem("wod__last-wheel-used");
+      if (lastWheelUsed === id) {
+        localStorage.removeItem("wod__last-wheel-used");
+      }
+      handleCreateNewWheel();
+    }
+
+    const newSavedSlices = { ...savedSlices };
+    delete newSavedSlices[id];
+    setSavedSlices(newSavedSlices);
+  };
+
+  const handleToggleSpinningWheel = () => {
+    setIsWheelSpinning(!isWheelSpinning);
+  };
+
+  return (
+    <WheelContext.Provider
+      value={{
+        state: {
+          wheelStatus,
+          wheelTitle,
+          currentWheelId,
+          slicesData,
+          wheelColors,
+          isWheelSpinning,
+          wheelThemeId,
+          isShowingWheelsList,
+          savedSlices,
+        },
+        handleToggleSpinningWheel,
+        handleSelectASavedWheel,
+        handleRemoveItemFromList,
+        handleCreateNewWheel,
+        handleShuffleSlices,
+        handleUpdateSlices,
+        handleChangeColor,
+        handleOrderSlices,
+        handleWheelTitle,
+        handleRemoveWheel,
+        handleSave,
+      }}
+    >
+      {children}
+    </WheelContext.Provider>
+  );
+};
+
+export const useWheelContext = () => {
+  const context = React.useContext(WheelContext);
+  if (!context) {
+    throw new Error("useWheelContext must be used within a WheelProvider");
+  }
+  return context;
+};
